97a5fe1ea9a4acc2aa0f19a2d4442e2bb5e6fcb8
AlanGitez <benitez.alanez@gmail.com>
2022-04-20 23:42:15 -0300

    Quedan pendientes.

--- commands.js
+++ commands.js
@@ -2,6 +2,8 @@
 function pruebas(){
     
 }
+// TOMA A PARTIR DEL SEGUNDO ELEMENTO DE UN ARRAY 
+// Y LO CONVIERTE EN STRING, LUEGO LO DEVUELVE.
 function argsToString(args){
     let str = args.join(" ");
     return str.substring(str.indexOf(" ")).trim();
@@ -13,23 +15,23 @@ function echo(cmd){
     process.stdout.write("\nprompt > ")
 }
 function cat(cmd=[]){
+    // NO LOS EJECUTA EN ORDEN, ALGUNOS TARDA MAS EN LEERLOS
+    // QUE OTROS, HABRA QUE CONFIGURARLO CON SETTIMEOUT
+    // O ALGO ASI, QUEDA PENDIENTE.
     cmd.splice(0, 1);
     cmd.forEach(file => {
         fs.readFile(file,"utf-8", function(err, data){
             if(err) process.stdout.write(err); 
-            process.stdout.write("\nprompt > ")
-            process.stdout.write(data.toString());
-            
+            process.stdout.write("\n"+data.toString());
+            process.stdout.write("\nprompt > ");            
         });
-        
     });
-    
 }
 function head(cmd){
-
+    // ES IMPOSIBLE HACER TODO ESTO EN UN DIA :(
 }
 function tail(cmd){
-
+    // ES IMPOSIBLE HACER ESTO TAMBIEN QUERIDO.
 }
 function pwd(){
     process.stdout.write(process.argv[1].toString());
