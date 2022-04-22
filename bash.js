327d0698651ee3b1d30d3b71e283c459e85289db
AlanGitez <benitez.alanez@gmail.com>
2022-04-22 19:59:57 -0300

    0-0

--- bash.js
+++ bash.js
@@ -2,10 +2,15 @@ function start(){
     process.stdout.write("prompt > ");
     process.stdin.on("data", function(data){
         let cmd = data.toString().trim().split(" ");
-
-        commands[cmd[0]](cmd);
+        
+        commands[cmd[0]](cmd,done);
+        process.stdout.write("\nprompt > ");
     });
-    process.stdout.write("\nprompt > ");
+    
+}
+function done(output){
+    process.stdout.write(output.toString());
+    process.stdout.write("\nprompt > "); 
 }
 start();
 let commands = require("./commands.js")
--- commands.js
+++ commands.js
@@ -8,47 +8,69 @@ function argsToString(args){
     let str = args.join(" ");
     return str.substring(str.indexOf(" ")).trim();
 }
-function echo(cmd){
+function curl(url,done){
+    request(argsToString(url), function(err, respuesta, body){
+        if(err) process.stdout.write(err); 
+        fs.writeFileSync("respuestaCurl.txt", respuesta);
+        fs.writeFileSync("bodyCurl.txt", body);
+        done(body);
+    })
+}
+function echo(cmd, done){
     if(cmd.length == 0) throw "No hay argumentos que imprimir";
     
-    process.stdout.write(argsToString(cmd));
-    process.stdout.write("\nprompt > ")
+    done(argsToString(cmd));
 }
-function cat(cmd=[]){
+function cat(files, done){
     // NO LOS EJECUTA EN ORDEN, ALGUNOS TARDA MAS EN LEERLOS
     // QUE OTROS, HABRA QUE CONFIGURARLO CON SETTIMEOUT
     // O ALGO ASI, QUEDA PENDIENTE.
-    cmd.splice(0, 1);
-    cmd.forEach(file => {
+    files.splice(0, 1);
+    files.forEach(file => {
         fs.readFile(file,"utf-8", function(err, data){
-            if(err) process.stdout.write(err); 
-            process.stdout.write("\n"+data.toString());
-            process.stdout.write("\nprompt > ");            
+            if(err) done(err);
+            done("\n"+data);            
         });
     });
 }
-function head(cmd){
-    // ES IMPOSIBLE HACER TODO ESTO EN UN DIA :(
+function head(files, done){
+    let responseLines = 5;
+    files.splice(0, 1);
+    files.forEach(file => {
+        fs.readFile(file,"utf-8", function(err, data){
+            if(err) done(err);  
+            let response = data.split("\n").splice(0, responseLines).join("\n"); 
+            done("\n"+response);        
+        });
+    });
 }
-function tail(cmd){
-    // ES IMPOSIBLE HACER ESTO TAMBIEN QUERIDO.
+function tail(files, done){
+    let responseLines = 5;
+    files.splice(0, 1);
+    files.forEach(file => {
+        fs.readFile(file,"utf-8", function(err, data){
+            if(err) done(err);
+            let response = data.split("\n");
+            response = response.splice((response.length-1)-responseLines, response.length-1).join("\n") 
+            done("\n"+response);        
+        });
+    });
 }
-function pwd(){
-    process.stdout.write(process.argv[1].toString());
-    process.stdout.write("\nprompt > ")
+function pwd(args, done){
+    done(process.argv[1]);
 }
-function date(){
-    process.stdout.write(new Date().toString());
-    process.stdout.write("\nprompt > ")
+function date(date, done){
+    done(new Date());
 }
-function ls(){
+function ls(file, done){
     fs.readdir("./", function(err, files){
         if(err) throw err;
         files.forEach(file => {
-            process.stdout.write(file.toString())
-            process.stdout.write("\nprompt > ")
+            done(file);
         });
     });
 }
 const fs = require("fs");
-module.exports = {pwd, date, ls, pruebas, echo, cat, head, tail};
+
+const request = require("request");
+module.exports = {pwd, date, ls, pruebas, echo, cat, head, tail, curl};
